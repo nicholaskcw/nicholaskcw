6a23489aedd3935086e117fca5a9c0b289bd9d51
Chin wei Kok <ckok@u80z03s02.42kl.edu.my>
2022-02-08 12:34:55 +0800

    5th commit

---
+++ P01/ex07/r_dwssap.sh
@@ -0,0 +1,12 @@
+cat /etc/passwd | \
+    grep -v '\#' | \
+    head -1
+    sed '1!n;d' | \
+    cut -d':' -f1 | \
+    rev | \
+    sort -r | \
+    awk 'NR>= ENVIRON["FT_LINE1"] && NR<= ENVIRON["FT_LINE2"]' | \
+    paste -s -d"," - | \
+    sed 's/,/, /g' | \
+    sed 's/$/./' | \
+    tr -d '\n'
